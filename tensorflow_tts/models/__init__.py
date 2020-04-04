254ac33302baf74ac6792d45ab02e004587f172e
dathudeptrai <nguyenquananhminh@gmail.com>
2020-04-04 17:16:27 +0700

    fix flake8 from __init__

--- tensorflow_tts/models/__init__.py
+++ tensorflow_tts/models/__init__.py
@@ -1,1 +1,3 @@
-from tensorflow_tts.models.melgan import *
+from tensorflow_tts.models.melgan import TFMelGANGenerator
+from tensorflow_tts.models.melgan import TFMelGANDiscriminator
+from tensorflow_tts.models.melgan import TFMelGANMultiScaleDiscriminator
