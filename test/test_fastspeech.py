145500d06437336f8f3c5a1ff476a7081ce80fde
dathudeptrai <nguyenquananhminh@gmail.com>
2020-04-28 17:10:23 +0700

    fix dependency soundfine on test fastspeech cause CI failed.

--- test/test_fastspeech.py
+++ test/test_fastspeech.py
@@ -4,12 +4,59 @@ import pytest
 import tensorflow as tf
 
 from tensorflow_tts.models import TFFastSpeech
-from tensorflow_tts.configs import FastSpeechConfig
 
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s (%(module)s:%(lineno)d) %(levelname)s: %(message)s")
+
+
+class FastSpeechConfig(object):
+    """Initialize FastSpeech Config."""
+
+    def __init__(
+            self,
+            vocab_size=200,
+            n_speakers=1,
+            hidden_size=384,
+            num_hidden_layers=2,
+            num_attention_heads=2,
+            intermediate_size=256,
+            intermediate_kernel_size=3,
+            num_duration_conv_layers=2,
+            duration_predictor_filters=128,
+            duration_predictor_kernel_sizes=3,
+            num_mels=80,
+            hidden_act="gelu",
+            hidden_dropout_prob=0.1,
+            attention_probs_dropout_prob=0.1,
+            duration_predictor_dropout_probs=0.1,
+            max_position_embeddings=2048,
+            initializer_range=0.02,
+            layer_norm_eps=1e-12,
+            output_attentions=False,
+            output_hidden_states=False):
+        """Init parameters for Fastspeech model."""
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.hidden_act = hidden_act
+        self.intermediate_size = intermediate_size
+        self.intermediate_kernel_size = intermediate_kernel_size
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.max_position_embeddings = max_position_embeddings
+        self.initializer_range = initializer_range
+        self.layer_norm_eps = layer_norm_eps
+        self.n_speakers = n_speakers
+        self.output_attentions = output_attentions
+        self.output_hidden_states = output_hidden_states
+        self.duration_predictor_dropout_probs = duration_predictor_dropout_probs
+        self.num_duration_conv_layers = num_duration_conv_layers
+        self.duration_predictor_filters = duration_predictor_filters
+        self.duration_predictor_kernel_sizes = duration_predictor_kernel_sizes
+        self.num_mels = num_mels
 
 
 @pytest.mark.parametrize(
