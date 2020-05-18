3af3371f37fe31f2f1c10e48ecd3c8a559c6fc3f
dathudeptrai <nguyenquananhminh@gmail.com>
2020-05-18 09:57:14 +0700

    adding MultiResolution STFT loss.

---
+++ tensorflow_tts/losses/stft.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Minh Nguyen (@dathudeptrai)
+# This code is modified from
+# https://github.com/kan-bayashi/ParallelWaveGAN/blob/master/parallel_wavegan/losses/stft_loss.py
+#  MIT License (https://opensource.org/licenses/MIT)
+
+"""STFT-based loss modules."""
+
+import tensorflow as tf
+
+
+class TFSpectralConvergence(tf.keras.layers.Layer):
+    """Spectral convergence loss."""
+
+    def __init__(self):
+        """Initialize."""
+        super().__init__()
+
+    def call(self, y_mag, x_mag):
+        """Calculate forward propagation.
+        Args:
+            y_mag (Tensor): Magnitude spectrogram of groundtruth signal (B, #frames, #freq_bins).
+            x_mag (Tensor): Magnitude spectrogram of predicted signal (B, #frames, #freq_bins).
+        Returns:
+            Tensor: Spectral convergence loss value.
+        """
+        return tf.norm(y_mag - x_mag, ord="fro", axis=(1, 2)) / tf.norm(y_mag, ord="fro", axis=(1, 2))
+
+
+class TFLogSTFTMagnitude(tf.keras.layers.Layer):
+    """Log STFT magnitude loss module."""
+
+    def __init__(self):
+        """Initialize."""
+        super().__init__()
+
+    def call(self, y_mag, x_mag):
+        """Calculate forward propagation.
+        Args:
+            y_mag (Tensor): Magnitude spectrogram of groundtruth signal (B, #frames, #freq_bins).
+            x_mag (Tensor): Magnitude spectrogram of predicted signal (B, #frames, #freq_bins).
+        Returns:
+            Tensor: Spectral convergence loss value.
+        """
+        return tf.math.log(tf.abs(y_mag) + 1e-9) - tf.math.log(tf.abs(x_mag) + 1e-9)
+
+
+class TFSTFT(tf.keras.layers.Layer):
+    """STFT loss module."""
+
+    def __init__(self, frame_length=1024, frame_step=120, fft_length=600):
+        """Initialize."""
+        super().__init__()
+        self.frame_length = frame_length
+        self.frame_step = frame_step
+        self.fft_length = fft_length
+        self.spectral_convergenge_loss = TFSpectralConvergence()
+        self.log_stft_magnitude_loss = TFLogSTFTMagnitude()
+
+    def call(self, y, x):
+        """Calculate forward propagation.
+        Args:
+            y (Tensor): Groundtruth signal (B, T).
+            x (Tensor): Predicted signal (B, T).
+        Returns:
+            Tensor: Spectral convergence loss value (pre-reduce).
+            Tensor: Log STFT magnitude loss value (pre-reduce).
+        """
+        x_mag = tf.abs(tf.signal.stft(signals=x,
+                                      frame_length=self.frame_length,
+                                      frame_step=self.frame_step,
+                                      fft_length=self.fft_length))
+        y_mag = tf.abs(tf.signal.stft(signals=y,
+                                      frame_length=self.frame_length,
+                                      frame_step=self.frame_step,
+                                      fft_length=self.fft_length))
+
+        # calculate loss
+        sc_loss = self.spectral_convergenge_loss(y_mag, x_mag)
+        mag_loss = self.log_stft_magnitude_loss(y_mag, x_mag)
+
+        return sc_loss, mag_loss
+
+
+class TFMultiResolutionSTFT(tf.keras.layers.Layer):
+    """Multi resolution STFT loss module."""
+
+    def __init__(self,
+                 frame_lengths=[1024, 2048, 512],
+                 frame_steps=[120, 240, 50],
+                 fft_lengths=[600, 1200, 240]):
+        """Initialize Multi resolution STFT loss module.
+        Args:
+            frame_lengths (list): List of FFT sizes.
+            frame_steps (list): List of hop sizes.
+            fft_lengths (list): List of window lengths.
+        """
+        super().__init__()
+        assert len(frame_lengths) == len(frame_steps) == len(fft_lengths)
+        self.stft_losses = []
+        for frame_length, frame_step, fft_length in zip(frame_lengths, frame_steps, fft_lengths):
+            self.stft_losses.append(TFSTFT(frame_length, frame_step, fft_length))
+
+    def call(self, y, x):
+        """Calculate forward propagation.
+        Args:
+            y (Tensor): Groundtruth signal (B, T).
+            x (Tensor): Predicted signal (B, T).
+        Returns:
+            Tensor: Multi resolution spectral convergence loss value.
+            Tensor: Multi resolution log STFT magnitude loss value.
+        """
+        sc_loss = 0.0
+        mag_loss = 0.0
+        for f in self.stft_losses:
+            sc_l, mag_l = f(y, x)
+            sc_loss += tf.reduce_mean(sc_l)
+            mag_loss += tf.reduce_mean(mag_l)
+
+        sc_loss /= len(self.stft_losses)
+        mag_loss /= len(self.stft_losses)
+
+        return sc_loss, mag_loss
