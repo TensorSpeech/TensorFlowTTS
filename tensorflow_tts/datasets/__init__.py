dbce18736d812e357b996c64195193a3622a14cc
dathudeptrai <nguyenquananhminh@gmail.com>
2020-05-30 23:56:32 +0700

    fix conflict

--- tensorflow_tts/datasets/__init__.py
+++ tensorflow_tts/datasets/__init__.py
@@ -2,4 +2,4 @@ from tensorflow_tts.datasets.abstract_dataset import AbstractDataset
 
 from tensorflow_tts.datasets.tacotron_dataset import CharactorMelDataset
 
-from tensorflow_tts.datasets.mel_dataset import
+from tensorflow_tts.datasets.mel_dataset import MelDataset
