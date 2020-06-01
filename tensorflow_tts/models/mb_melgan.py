0234a57b768dbba386ba0e7122b95c7ebc49e372
dathudeptrai <nguyenquananhminh@gmail.com>
2020-06-01 23:13:04 +0700

    add pqmf layers compatible with pytorch, add test pqmf

--- tensorflow_tts/models/mb_melgan.py
+++ tensorflow_tts/models/mb_melgan.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2020 The MelGAN Authors and Minh Nguyen (@dathudeptrai)
+# Copyright 2020 The Multi-band MelGAN Authors , Minh Nguyen (@dathudeptrai) and Tomoki Hayashi (@kan-bayashi)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,4 +12,131 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Multi-band MelGAN Modules."""
+"""Multi-band MelGAN Modules. Compatible with
+https://github.com/kan-bayashi/ParallelWaveGAN/blob/master/parallel_wavegan/layers/pqmf.py."""
+
+import tensorflow as tf
+import numpy as np
+from scipy.signal import kaiser
+
+from tensorflow_tts.models import TFMelGANGenerator
+
+
+def design_prototype_filter(taps=62, cutoff_ratio=0.15, beta=9.0):
+    """Design prototype filter for PQMF.
+    This method is based on `A Kaiser window approach for the design of prototype
+    filters of cosine modulated filterbanks`_.
+    Args:
+        taps (int): The number of filter taps.
+        cutoff_ratio (float): Cut-off frequency ratio.
+        beta (float): Beta coefficient for kaiser window.
+    Returns:
+        ndarray: Impluse response of prototype filter (taps + 1,).
+    .. _`A Kaiser window approach for the design of prototype filters of cosine modulated filterbanks`:
+        https://ieeexplore.ieee.org/abstract/document/681427
+    """
+    # check the arguments are valid
+    assert taps % 2 == 0, "The number of taps mush be even number."
+    assert 0.0 < cutoff_ratio < 1.0, "Cutoff ratio must be > 0.0 and < 1.0."
+
+    # make initial filter
+    omega_c = np.pi * cutoff_ratio
+    with np.errstate(invalid='ignore'):
+        h_i = np.sin(omega_c * (np.arange(taps + 1) - 0.5 * taps)) \
+            / (np.pi * (np.arange(taps + 1) - 0.5 * taps))
+    # fix nan due to indeterminate form
+    h_i[taps // 2] = np.cos(0) * cutoff_ratio
+
+    # apply kaiser window
+    w = kaiser(taps + 1, beta)
+    h = h_i * w
+
+    return h
+
+
+class TFPQMF(tf.keras.layers.Layer):
+    """PQMF module."""
+
+    def __init__(self, subbands=4, taps=62, cutoff_ratio=0.15, beta=9.0, **kwargs):
+        """Initilize PQMF module.
+        Args:
+            subbands (int): The number of subbands.
+            taps (int): The number of filter taps.
+            cutoff_ratio (float): Cut-off frequency ratio.
+            beta (float): Beta coefficient for kaiser window.
+        """
+        super().__init__(**kwargs)
+
+        # define filter coefficient
+        h_proto = design_prototype_filter(taps, cutoff_ratio, beta)
+        h_analysis = np.zeros((subbands, len(h_proto)))
+        h_synthesis = np.zeros((subbands, len(h_proto)))
+        for k in range(subbands):
+            h_analysis[k] = 2 * h_proto * np.cos(
+                (2 * k + 1) * (np.pi / (2 * subbands)) *
+                (np.arange(taps + 1) - ((taps - 1) / 2)) +
+                (-1) ** k * np.pi / 4)
+            h_synthesis[k] = 2 * h_proto * np.cos(
+                (2 * k + 1) * (np.pi / (2 * subbands)) *
+                (np.arange(taps + 1) - ((taps - 1) / 2)) -
+                (-1) ** k * np.pi / 4)
+
+        # [subbands, 1, taps + 1] == [filter_width, in_channels, out_channels]
+        analysis_filter = np.expand_dims(h_analysis, 1)
+        analysis_filter = np.transpose(analysis_filter, (2, 1, 0))
+
+        synthesis_filter = np.expand_dims(h_analysis, 0)
+        synthesis_filter = np.transpose(synthesis_filter, (2, 1, 0))
+
+        # filter for downsampling & upsampling
+        updown_filter = np.zeros((subbands, subbands, subbands), dtype=np.float32)
+        for k in range(subbands):
+            updown_filter[0, k, k] = 1.0
+
+        self.subbands = subbands
+        self.taps = taps
+        self.analysis_filter = analysis_filter.astype(np.float32)
+        self.synthesis_filter = synthesis_filter.astype(np.float32)
+        self.updown_filter = updown_filter.astype(np.float32)
+
+    @tf.function(experimental_relax_shapes=True)
+    def analysis(self, x):
+        """Analysis with PQMF.
+        Args:
+            x (Tensor): Input tensor (B, T, 1).
+        Returns:
+            Tensor: Output tensor (B, T // subbands, subbands).
+        """
+        x = tf.pad(x, [[0, 0], [self.taps // 2, self.taps // 2], [0, 0]])
+        x = tf.nn.conv1d(x, self.analysis_filter, stride=1, padding='VALID')
+        x = tf.nn.conv1d(x, self.updown_filter, stride=self.subbands, padding='VALID')
+        return x
+
+    @tf.function(experimental_relax_shapes=True)
+    def synthesis(self, x):
+        """Synthesis with PQMF.
+        Args:
+            x (Tensor): Input tensor (B, T // subbands, subbands).
+        Returns:
+            Tensor: Output tensor (B, T, 1).
+        """
+        x = tf.nn.conv1d_transpose(x,
+                                   self.updown_filter * self.subbands,
+                                   strides=self.subbands,
+                                   output_shape=(tf.shape(x)[0], tf.shape(x)[1] * self.subbands, self.subbands))
+        x = tf.pad(x, [[0, 0], [self.taps // 2, self.taps // 2], [0, 0]])
+        return tf.nn.conv1d(x, self.synthesis_filter, stride=1, padding="VALID")
+
+
+class TFMultiBandMelGANGenerator(TFMelGANGenerator):
+    """Tensorflow Multi-band MelGAN generator module."""
+
+    def __init__(self, config, **kwargs):
+        super().__init__(config, **kwargs)
+        self.pqmf = TFPQMF(
+            subbands=config.subbands,
+            taps=config.taps,
+            cutoff_ratio=config.cutoff_ratio,
+            beta=config.beta,
+            name="pqmf"
+        )
---
+++ test/test_mb_melgan.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Minh Nguyen (@dathudeptrai)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+
+import numpy as np
+import pytest
+import tensorflow as tf
+
+from tensorflow_tts.configs import MultiBandMelGANGeneratorConfig
+from tensorflow_tts.models import TFMultiBandMelGANGenerator
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s (%(module)s:%(lineno)d) %(levelname)s: %(message)s")
+
+
+def make_multi_band_melgan_generator_args(**kwargs):
+    defaults = dict(
+        out_channels=1,
+        kernel_size=7,
+        filters=512,
+        use_bias=True,
+        upsample_scales=[8, 8, 2, 2],
+        stack_kernel_size=3,
+        stacks=3,
+        nonlinear_activation="LeakyReLU",
+        nonlinear_activation_params={"alpha": 0.2},
+        padding_type="REFLECT",
+        subbands=4,
+        tabs=62,
+        cutoff_ratio=0.15,
+        beta=9.0
+    )
+    defaults.update(kwargs)
+    return defaults
+
+
+@pytest.mark.parametrize(
+    "dict_g", [
+        {"subbands": 4, "upsample_scales": [2, 4, 8], "stacks": 4, "out_channels": 4},
+        {"subbands": 4, "upsample_scales": [4, 4, 4], "stacks": 5, "out_channels": 4}
+    ])
+def test_multi_band_melgan(dict_g):
+    args_g = make_multi_band_melgan_generator_args(**dict_g)
+    args_g = MultiBandMelGANGeneratorConfig(**args_g)
+    generator = TFMultiBandMelGANGenerator(args_g, name="multi_band_melgan")
+    generator._build()
+
+    fake_mels = tf.random.uniform(shape=[1, 100, 80], dtype=tf.float32)
+    fake_y = tf.random.uniform(shape=[1, 100 * 256, 1], dtype=tf.float32)
+    y_hat_subbands = generator(fake_mels)
+
+    y_hat = generator.pqmf.synthesis(y_hat_subbands)
+    y_subbands = generator.pqmf.analysis(fake_y)
+
+    assert np.shape(y_subbands) == np.shape(y_hat_subbands)
+    assert np.shape(fake_y) == np.shape(y_hat)
