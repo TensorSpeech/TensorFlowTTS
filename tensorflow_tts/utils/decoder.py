d8f00cd887144ce27e52fb97115be918ee47c7e8
Jae H. Yoo <40815393+jaeyoo@users.noreply.github.com>
2020-07-04 22:25:35 +0900

    Update decoder.py

--- tensorflow_tts/utils/decoder.py
+++ tensorflow_tts/utils/decoder.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import tensorflow as tf
+
 from tensorflow_addons.utils.types import Number
 from tensorflow_addons.utils.types import TensorLike
 from tensorflow_addons.seq2seq.decoder import (
