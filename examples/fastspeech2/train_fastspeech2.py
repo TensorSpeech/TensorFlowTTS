b32d64f2102363ccbf37ec60a8e7d372e4501b00
ZDisket <xml506ok@gmail.com>
2020-07-10 20:41:57 -0300

    Fix memory leak on validation by forcing garbage collector

--- examples/fastspeech2/train_fastspeech2.py
+++ examples/fastspeech2/train_fastspeech2.py
@@ -29,6 +29,7 @@ import argparse
 import logging
 import os
 import sys
+import gc
 sys.path.append(".")
 
 
@@ -64,6 +65,7 @@ class FastSpeech2Trainer(FastSpeechTrainer):
         self.init_train_eval_metrics(self.list_metrics_name)
         self.reset_states_train()
         self.reset_states_eval()
+        gc.collect()
 
         self.config = config
 
