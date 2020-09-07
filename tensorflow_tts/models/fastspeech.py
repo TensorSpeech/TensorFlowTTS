30d7bcc186b0bff9ef1bfc5c512ba5e28b96b841
dathudeptrai <nguyenquananhminh@gmail.com>
2020-09-07 12:58:18 +0700

    🔧 Fix (#251).

--- tensorflow_tts/models/fastspeech.py
+++ tensorflow_tts/models/fastspeech.py
@@ -69,8 +69,8 @@ class TFEmbedding(tf.keras.layers.Embedding):
         super().__init__(*args, **kwargs)
 
     def call(self, inputs):
-        inputs = tf.cast(tf.expand_dims(inputs, -1), tf.int32)
-        outputs = tf.gather_nd(self.embeddings, inputs)
+        inputs = tf.cast(inputs, tf.int32)
+        outputs = tf.gather(self.embeddings, inputs)
         return outputs
 
 
