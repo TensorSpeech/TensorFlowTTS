cc2f7eee69c40c73cfc24798efd2f059971a8aed
machineko <42299606+machineko@users.noreply.github.com>
2020-11-06 03:15:44 +0100

    fix black problems

--- tensorflow_tts/processor/libritts.py
+++ tensorflow_tts/processor/libritts.py
@@ -114,5 +114,6 @@ class LibriTTSProcessor(BaseProcessor):
                         "@END"
                     )  # TODO try learning without end token and compare results
                 break
-            data.append("@" + txt) if txt != " "
+            if txt != " ":
+                data.append("@" + txt) 
         return data
