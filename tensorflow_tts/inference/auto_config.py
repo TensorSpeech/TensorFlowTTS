0a677c47ebd0337b18066c6816275811c5b70af3
dathudeptrai <nguyenquananhminh@gmail.com>
2020-11-24 11:43:10 +0700

    ✋ Add hifigan to autoconfig and TFautomodel.

--- tensorflow_tts/inference/auto_config.py
+++ tensorflow_tts/inference/auto_config.py
@@ -23,6 +23,7 @@ from tensorflow_tts.configs import (
     FastSpeech2Config,
     MelGANGeneratorConfig,
     MultiBandMelGANGeneratorConfig,
+    HifiGANGeneratorConfig,
     Tacotron2Config,
     ParallelWaveGANGeneratorConfig,
 )
@@ -33,6 +34,7 @@ CONFIG_MAPPING = OrderedDict(
         ("fastspeech2", FastSpeech2Config),
         ("multiband_melgan_generator", MultiBandMelGANGeneratorConfig),
         ("melgan_generator", MelGANGeneratorConfig),
+        ("hifigan_generator", HifiGANGeneratorConfig),
         ("tacotron2", Tacotron2Config),
         ("parallel_wavegan_generator", ParallelWaveGANGeneratorConfig)
     ]
